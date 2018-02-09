eedf111043b785b16d07c14358a89eb37c183952
Akram <ak4am.mo@gmail.com>
2018-02-08 19:26:44 -0500

    Add the entire website, need to add the right text and see if the contact page work

---
+++ php/contact.php
@@ -0,0 +1,34 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+$from = 'From: Cynergie Website';
+$to = 'akram-mo@hotmail.com';
+
+$body = "From: $name\n E-mail: $email \n Phone: $phone \n Subject:
+$subject \n Message: $message";
+
+if($_POST['submit']){
+
+  if($name != '' || $email != '' || $subject != '' || $message != ''){
+
+    if(mail ( $to, $subject, $body, $from)){
+
+      echo '<p> Votre message a été envoyé</p>';
+
+    }else {
+
+      echo '<p> Le message n\'a pas été envoyé, une erreur s\'est produite ! </p>';
+
+    }
+  }else{
+
+    echo '<p> Des champs sont requis !</p>';
+
+  }
+
+}
+
+?>
